f9fd821cb0ba145083eac4e78451903a87410f9d
Brian Denicola <bjd145@yahoo.com>
2020-09-29 15:01:06 -0500

    inital code base of blazor app

---
+++ src/ui/Program.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Net.Http;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Text;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace cqrs_ui
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+            builder.Services.AddScoped(sp => new HttpClient {});
+            builder.RootComponents.Add<App>("app");
+            await builder.Build().RunAsync();
+        }
+    }
+}
