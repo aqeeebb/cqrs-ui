1131a5d5cd55b44feae9f95773a28167fd6c9a56
Brian Denicola <bjd145@yahoo.com>
2020-09-29 16:32:47 -0500

    Slightly improved mock

--- src/mock-api/passwords-get/index.js
+++ src/mock-api/passwords-get/index.js
@@ -1,15 +1,20 @@
 module.exports = async function (context, req) {
 
     var keyId = context.bindingData.keyId;
-    
+
+    const key = () => {
+        let rnd   = Math.floor(Math.random() * Math.floor(Number.MAX_SAFE_INTEGER)).toString()
+        return Buffer.from(rnd).toString('base64')
+    }
+
     responseMessage = {
         keyId:          keyId,
-        key:            Math.floor(Math.random() * Math.floor(Number.MAX_SAFE_INTEGER )),
+        key:            key(),
         readHost:       "reader-host",
         writeHost:      "writer-host",
         readRegion:     "eastus2",
         writeRegion:    "ukwest",
-        timeStamp:      Date.now()
+        timeStamp:      Date.now().toString()
     };
     
     context.res = {
